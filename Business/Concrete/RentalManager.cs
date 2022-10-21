f89de01923825587e1c13e8c96d9981af3913627
turkogluhakan613@gmail.com <turkogluhakan613@gmail.com>
2022-10-22 02:36:43 +0300

    CRUD operations are written.

---
+++ Business/Concrete/RentalManager.cs
@@ -0,0 +1,47 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class RentalManager : IRentalService
+    {
+        private IRentalDal _rentalDal;
+
+        public RentalManager(IRentalDal rentalDal)
+        {
+            _rentalDal = rentalDal;
+        }
+
+        public IResult Add(Rental rental)
+        {
+            _rentalDal.Add(rental);
+
+            return new SuccessResult();
+        }
+
+        public IResult Delete(Rental rental)
+        {
+            _rentalDal.Delete(rental);
+
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<Rental>> GetAll()
+        {
+            var data = _rentalDal.GetAll();
+            return new SuccessDataResult<List<Rental>>(data);
+        }
+
+        public IResult Update(Rental rental)
+        {
+            _rentalDal.Update(rental);
+
+            return new SuccessResult();
+        }
+    }
+}
