011aaf77374edb61119b6079047d2aee2773ebcf
turkogluhakan613@gmail.com <turkogluhakan613@gmail.com>
2022-10-22 02:25:57 +0300

    Added User, Customer, Rental objects. Required DAL classes have been created.

---
+++ DataAccess/Concrete/EntityFramework/EfRentalDal.cs
@@ -0,0 +1,14 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Context;
+using Entities.Concrete;
+
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfRentalDal:EfEntityRepositoryBase<Rental,RentACarContext>,IRentalDal
+    {
+
+    }
+
+}
